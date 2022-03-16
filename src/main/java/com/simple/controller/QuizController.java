c43e658a6c872255e96af0b8aabffd7c70c184f6
inwoo1 <coninwoo@gmail.com>
2022-03-16 18:20:47 +0900

    20220316 Spring2

--- src/main/java/com/simple/controller/QuizController.java
+++ src/main/java/com/simple/controller/QuizController.java
@@ -2,6 +2,7 @@ package com.simple.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.simple.command.QuizVO;
@@ -20,17 +21,28 @@ public class QuizController {
 	}
 	
 	@RequestMapping("/sendBirth")
-	public String sendBirth(QuizVO vo, Model model) {
+//	public String sendBirth(QuizVO vo, Model model) {
+//		
+//		System.out.println(	vo.getYear() +
+//				(Integer.parseInt(vo.getMonth()) < 10 ? "0"+Integer.parseInt(vo.getMonth()) : Integer.parseInt(vo.getMonth()))+ 
+//				(Integer.parseInt(vo.getDay()) < 10 ? "0"+Integer.parseInt(vo.getDay()) : Integer.parseInt(vo.getDay())));
+//		model.addAttribute("year", vo.getYear());
+//		model.addAttribute("month", vo.getMonth());
+//		model.addAttribute("day", vo.getDay());
+//		 
+//		return "quiz/quiz01_ok";
+//		
+//	}
+	
+	public String sendBirth(@ModelAttribute("year") String year,
+							@ModelAttribute("month") String month,
+							@ModelAttribute("day") String day) {
 		
-		System.out.println(	vo.getYear() +
-				(Integer.parseInt(vo.getMonth()) < 10 ? "0"+Integer.parseInt(vo.getMonth()) : Integer.parseInt(vo.getMonth()))+ 
-				(Integer.parseInt(vo.getDay()) < 10 ? "0"+Integer.parseInt(vo.getDay()) : Integer.parseInt(vo.getDay())));
-		model.addAttribute("year", vo.getYear());
-		model.addAttribute("month", vo.getMonth());
-		model.addAttribute("day", vo.getDay());
-		 
+		System.out.println(year +
+						(Integer.parseInt(month) < 10 ? "0"+Integer.parseInt(month) : Integer.parseInt(month) )+
+						(Integer.parseInt(day) < 10 ? "0"+Integer.parseInt(day) : Integer.parseInt(day) ));
+		
 		return "quiz/quiz01_ok";
-		
 	}
 
 }
